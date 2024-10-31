ecb34d86d71b4919af033089a4db88f35f92585a
Michael Mayer <michael.mayer@posit.co>
2024-10-31 18:11:11 +0000

    update clustermq

--- clustermq/compute.R
+++ clustermq/compute.R
@@ -1,27 +1,27 @@
 # example courtesy of Andrie de Vries (andrie@posit.co)
+
 compute <- function(n) {
-  library(palmerpenguins)
-  library(dplyr)
-  peng <- penguins %>% 
-    filter(!is.na(species) & !is.na(sex)) %>%
-    mutate(
-      species = as.factor(species),
-      sex = as.factor(sex)
-    ) %>% 
-    sample_n(333)
-  res<-glm(body_mass_g ~ species + sex, data = peng)
+  peng <- penguins %>%
+    mutate(species = as.factor(species), sex = as.factor(sex)) %>%
+    sample_n(100)
+  glm(body_mass_g ~ species + sex, data = peng)
 }
 
-library(clustermq) 
-samples=2
+library(clustermq)
+
+samples <- 1000
+
 # run a big-ish compute job
-res <- clustermq::Q(compute, n = 1:samples, n_jobs = 1,chunk_size=10)
+res <- clustermq::Q(
+  compute,
+  n = 1:samples,
+  n_jobs = 20,
+  chunk_size = 10,
+  pkgs = c("dplyr", "palmerpenguins")
+)
 
 # create new data
-new_dat <- tibble::tribble(
-  ~species, ~sex,
-  "Chinstrap", "male"
-)
+new_dat <- tibble::tribble(~ species, ~ sex, "Chinstrap", "male")
 
 # create prediction for each of the models
 library(purrr)
@@ -31,7 +31,6 @@ preds <- tibble(mass = map_dbl(res, predict, new_dat))
 
 # plot the result
 library(ggplot2)
-ggplot(preds, aes(x = mass)) + 
-  geom_histogram(bins=samples/50) +
+ggplot(preds, aes(x = mass)) +
+  geom_histogram(bins = samples / 50) +
   ggtitle("Ensemble model prediction of mass of male Chinstrap penguins")
-
